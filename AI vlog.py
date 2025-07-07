ab4c7e82cb96379932e76c99c0e3de84cdcb7f8d
maharfs <fsfaheem173@gmail.com>
2025-07-07 11:17:08 +0500

    Update AI vlog.py

--- AI vlog.py
+++ AI vlog.py
@@ -3,7 +3,6 @@ import streamlit as st
 from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 
-Set your YouTube API key
 API_KEY ="AIzaSyCIYqDE9Zn8Q9pUg_2hSampeRzPINn_FjQ"
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
